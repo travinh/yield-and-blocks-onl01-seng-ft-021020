d836c9b4a4ebd871e395ba25790e39aacc953c4c
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-14 06:41:43 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,6 +4,7 @@ def hello_t(array)
     yield array[i]
     i = i+1
   end
+  array
 end
 
 # call your method here!
@@ -11,7 +12,6 @@ hello_t(["Tim","Tom","Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
-  array
 end
 
   
